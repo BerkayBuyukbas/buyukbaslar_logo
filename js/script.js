dc652bdcf8b6b3885681915eb5a2c8433d7efd43
BerkayBuyukbas <122968469+BerkayBuyukbas@users.noreply.github.com>
2023-01-19 10:22:32 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,8 @@
 const searchForm = document.querySelector(".search-form");
-const cartItem = document.querySelector(".cart-items-container");
 const navbar = document.querySelector(".navbar");
 
 //! buttons
 const searchBtn = document.querySelector("#search-btn");
-const cartBtn = document.querySelector("#cart-btn");
 const menuBtn = document.querySelector("#menu-btn");
 
 searchBtn.addEventListener("click", function () {
@@ -15,18 +13,6 @@ searchBtn.addEventListener("click", function () {
       !e.composedPath().includes(searchForm)
     ) {
       searchForm.classList.remove("active");
-    }
-  });
-});
-
-cartBtn.addEventListener("click", function () {
-  cartItem.classList.toggle("active");
-  document.addEventListener("click", function (e) {
-    if (
-      !e.composedPath().includes(cartBtn) &&
-      !e.composedPath().includes(cartItem)
-    ) {
-      cartItem.classList.remove("active");
     }
   });
 });
